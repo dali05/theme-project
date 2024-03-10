fdd87b5f79acdf2cac9a2f03e099d0d6dce4f313
dali05 <dali.laaroussi@gmail.Com>
2024-03-10 22:57:46 +0100

    first commit

---
+++ projects/theme-manager/webpack.config.js
@@ -0,0 +1,57 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "themeManager",
+    publicPath: "auto"
+  },
+  optimization: {
+    runtimeChunk: false
+  },   
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+        library: { type: "module" },
+
+        // For remotes (please adjust)
+        // name: "themeManager",
+        // filename: "remoteEntry.js",
+        // exposes: {
+        //     './Component': './projects/theme-manager/src/app/app.component.ts',
+        // },        
+        
+        // For hosts (please adjust)
+        // remotes: {
+        //     "partener1": "http://localhost:4200/remoteEntry.js",
+        //     "partener2": "http://localhost:4200/remoteEntry.js",
+
+        // },
+
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+        
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
